2337b3325749ff22507e64d6d609a1d5592e6fa5
DANI <DANI@DANI-PC>
2013-04-11 09:56:48 +0200

    commint inicial del proyecto

---
+++ src/Mipruebagit/pruebagit.java
@@ -0,0 +1,14 @@
+package Mipruebagit;
+
+public class pruebagit {
+
+	/**clase para ejemplo de uso de git
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("A ver si Git nos gusta");
+
+	}
+
+}
